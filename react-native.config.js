d320be790bff7c7b3a26c99dabed07d865c090af
jlemdo <jlelpella@gmail.com>
2025-07-01 22:51:06 -0600

    first commits

--- react-native.config.js
+++ react-native.config.js
@@ -1,7 +1,16 @@
+// react-native.config.js
+
 module.exports = {
   project: {
     ios: {},
     android: {},
   },
   assets: ['./src/assets/fonts'],
+  dependencies: {
+    'react-native-maps': {
+      platforms: {
+        ios: null,   // ← desactiva el enlace automático de iOS
+      },
+    },
+  },
 };
